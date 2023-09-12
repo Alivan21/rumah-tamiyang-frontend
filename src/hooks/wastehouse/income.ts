761ab0e25d553bd5fc4dd564cdc6ee2fe7d1fb46
Fauzan Pradana <fauzanpr.06@gmail.com>
2023-09-13 06:57:33 +0700

    feat(wastehouse_income): read and delete on wastehouse income

---
+++ src/hooks/wastehouse/income.ts
@@ -0,0 +1,34 @@
+import { httpClient } from "@/utils/http";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+export type incomeType = {
+    id: number;
+    date: string;
+    amount: number;
+};
+
+type mutationProps = {
+    onSuccess: () => void,
+    onError: () => void
+};
+
+export function useGetAllWasteHouseIncomeQuery() {
+    return useQuery({
+        queryKey: ["wastehouse-income"],
+        queryFn: async () => {
+            const res = await httpClient.get("/waste-house/income?page=1&perPage=10");
+            return res.data;
+        }
+    });
+};
+
+export function useDeleteWasteHouseIncomeMutation(id: number, { onSuccess, onError }: mutationProps) {
+    return useMutation({
+        mutationFn: async () => {
+            const res = await httpClient.delete(`/waste-house/income/${id}`);
+            return res;
+        },
+        onSuccess,
+        onError
+    })
+};
--- src/pages/wastehouse/income/index.tsx
+++ src/pages/wastehouse/income/index.tsx
@@ -1,8 +1,40 @@
 import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 import { BiSolidPlusCircle, BiSolidShow, BiSolidEdit, BiSolidTrash } from "react-icons/bi";
 import { Link } from "react-router-dom";
+import { incomeType, useDeleteWasteHouseIncomeMutation, useGetAllWasteHouseIncomeQuery } from "@/hooks/wastehouse/income";
+import { useToast } from "@/components/ui/use-toast";
+import DeleteModal from "@/components/modals/DeleteModal";
 
 function WasteHouseIncome() {
+    const { toast } = useToast();
+
+    const [idDeleted, setIdDeleted] = useState<number>(-1);
+
+    const { data, refetch } = useGetAllWasteHouseIncomeQuery();
+    const { mutate: onDelete } = useDeleteWasteHouseIncomeMutation(idDeleted, {
+        onSuccess: () => {
+            toast({
+                title: "Data berhasil dihapus",
+                variant: "success"
+            });
+            refetch();
+        },
+        onError: () => {
+            toast({
+                title: "Data gagal dihapus",
+                variant: "destructive"
+            });
+        }
+    });
+    const deleteHandler = (id: number) => {
+        event?.preventDefault();
+        setIdDeleted(id);
+        onDelete();
+    }
+    useEffect(() => {
+        refetch();
+    }, [data, refetch]);
     return (
         <>
             <div className="flex items-center justify-between">
@@ -32,11 +64,11 @@ function WasteHouseIncome() {
                         </tr>
                     </thead>
                     <tbody>
-                        {[1, 2, 3].map(num => (
-                            <tr key={num} className="border-b bg-white">
-                                <td className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 text-center">1</td>
-                                <td className="px-6 py-4 text-center">12/12/1212</td>
-                                <td className="px-6 py-4 text-center">Rp1000000</td>
+                        {data?.data.map((income: incomeType, index: number) => (
+                            <tr key={income.id} className="border-b bg-white">
+                                <td className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 text-center">{ index+1 }</td>
+                                <td className="px-6 py-4 text-center">{ income.date }</td>
+                                <td className="px-6 py-4 text-center">Rp{ income.amount }</td>
                                 <td className="m-auto mt-2 flex w-fit items-center gap-3">
                                     <Link
                                         to={`/wastehouse/income/${1}`}
@@ -45,16 +77,12 @@ function WasteHouseIncome() {
                                         <BiSolidShow color="white" size={20} />
                                     </Link>
                                     <Link
-                                        to={`/wastehouse/income/edit/${1}`}
+                                        to={`/wastehouse/income/edit/${income.id}`}
                                         className="m-auto w-fit cursor-pointer rounded-lg bg-blue-600 p-1"
                                     >
                                         <BiSolidEdit color="white" size={20} />
                                     </Link>
-                                    <Button
-                                        className="m-auto w-fit h-fit cursor-pointer rounded-lg bg-red-500 p-1"
-                                    >
-                                        <BiSolidTrash color="white" size={20} />
-                                    </Button>
+                                    <DeleteModal deleteHandler={() => deleteHandler(income.id)} />
                                 </td>
                             </tr>
                         )
